debe098d64549edbd69a2ce247fe29dae04657f5
matedge <mat.edge@gmail.com>
2018-02-27 14:29:01 +1100

    added event handling

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { Component } from "react";
 
-const SearchBar = () => {
-    return <input />
-};
+//  functional comoponent 
+
+// const SearchBar = () => {
+//     return <input />
+// };
+
+class SearchBar extends Component {
+    render() {
+        return <input onChange={event => console.log(event.target.value)} />;
+    }
+
+}
 
 export default SearchBar;
